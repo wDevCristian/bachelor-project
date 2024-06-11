c4f826f123523477e1f777515b1afde50f96f534
wDevCristian <cristian.gurduza@gmail.com>
2024-06-11 23:01:58 +0300

    fead(forgot): add main.jsx to the previous commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import UserStore from "./store/UserStore.js";
+import MenuItemActiveStore from "./store/MenuItemActiveStore.js";
 
 export const Context = createContext(null);
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Context.Provider
       value={{
         user: new UserStore(),
+        menuItemActive: new MenuItemActiveStore(),
       }}
     >
       <BrowserRouter>
